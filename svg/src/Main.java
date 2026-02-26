7c496c32da9e588c654336cff755a4aea0d63430
student <kammaz7n@gmail.com>
2026-02-26 12:03:12 +0100

    svg z2

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -4,8 +4,12 @@ public class Main {
     Point point = new Point();
     point.x = 6.7F;
     point.y = 4.1F;
-        System.out.println(point.toSvg());
-    //System.out.println(point.x + " " + point.y);
+
+    Point point2 = point.translated(2f, 2f);
+        System.out.println(point2);
+    point.translate(point.x, point.y);
+        System.out.println(point);
+
 
 
     }
--- svg/src/Point.java
+++ svg/src/Point.java
@@ -13,7 +13,18 @@ public class Point {
     public String toSvg(){
         return String.format(Locale.ENGLISH, "<circle r=\"5\" cx=\"%f\" cy=\"%f\" fill=\"red\" />", x, y);
     }
+    public void translate(float dx, float dy){
+        x+=dx;
+        y+=dy;
+    }
 
+    public Point translated(float dx, float dy){
+        Point point = new Point();
+        point.x=this.x+dx;
+        point.y=this.y+dy;
+
+        return point;
+    }
 
 }
 
