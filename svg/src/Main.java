9020401bc27552652e7ebe022b3f3e5d23d4a38e
student <kammaz7n@gmail.com>
2026-02-26 11:55:19 +0100

    svg z1

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Hello world");
+    Point point = new Point();
+    point.x = 6.7F;
+    point.y = 4.1F;
+        System.out.println(point.toSvg());
+    //System.out.println(point.x + " " + point.y);
+
+
     }
 
 }
---
+++ svg/src/Point.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+
+public class Point {
+    public float x;
+    public float y;
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+    public String toSvg(){
+        return String.format(Locale.ENGLISH, "<circle r=\"5\" cx=\"%f\" cy=\"%f\" fill=\"red\" />", x, y);
+    }
+
+
+}
+
