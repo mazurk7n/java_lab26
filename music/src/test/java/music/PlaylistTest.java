592915897dcfd8bf4ab8c57907721937dc4b5dee
mazurk7n <kammaz7n@gmail.com>
2026-05-21 10:42:39 +0200

    tests 1a-1c

--- music/src/test/java/music/PlaylistTest.java
+++ music/src/test/java/music/PlaylistTest.java
@@ -1,4 +1,18 @@
 package music;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PlaylistTest {
+    @Test
+    public void testEmptyPlaylist(){
+        Playlist playlist = new Playlist();
+        assertTrue (playlist.isEmpty());
+    }
+    @Test
+    public void testSingleElement(){
+        Playlist playlist = new Playlist();
+        playlist.add(new Song("Daria Zawiałow", "Pank fu", 183));
+        assertEquals(1, playlist.size());
+    }
 }
