e2e0a30d948c3cc4e465bd43c40b0269e244857b
mazurk7n <kammaz7n@gmail.com>
2026-05-28 12:12:54 +0200

    cos

---
+++ chat/src/Server.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+public class Server {
+    private ServerSocket serverSocket;
+    private HashMap<String,ClientHandler> handlers = new HashMap<>();
+
+    public Server(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+    }
+
+    public void listen() throws IOException {
+        System.out.println("Server started");
+        while(true) {
+            Socket socket = serverSocket.accept();
+            ClientHandler handler = new ClientHandler(socket, this);
+            Thread thread = new Thread(handler);
+            thread.start();
+            handlers.put(handler.getLogin(),handler);
+        }
+    }
+
+    public void online(ClientHandler client) {
+        client.send(
+                handlers.values().stream()
+                        .map(ClientHandler::getLogin).collect(Collectors.joining("\n"))
+        );
+    }
+
+    public void broadcast(String message, ClientHandler sender) {
+        handlers.values().stream()
+                .filter(receiver -> receiver != sender)
+                .forEach(handler -> handler.send(message));
+    }
+}
