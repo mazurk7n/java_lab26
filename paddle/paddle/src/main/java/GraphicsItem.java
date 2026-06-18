c5bb29050419c43399686985c1d10c2db423b10e
mazurk7n <kammaz7n@gmail.com>
2026-06-18 10:35:23 +0200

    z1

--- paddle/paddle/src/main/java/GraphicsItem.java
+++ paddle/paddle/src/main/java/GraphicsItem.java
@@ -1,2 +1,32 @@
-public class GraphicsItem {
+import java.awt.*;
+
+public abstract class GraphicsItem {
+    protected static double canvasWidth;
+    protected static double canvasHeight;
+    protected double x;
+    protected double y;
+    protected double witdth;
+    protected double height;
+    public static void setCanvasSize(double canvasWidth, double canvasHeight) {
+        GraphicsItem.canvasWidth = canvasWidth;
+        GraphicsItem.canvasHeight = canvasHeight;
+    }
+
+    public abstract void draw(Graphics2D graphics2D);
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getWitdth() {
+        return witdth;
+    }
+
+    public double getHeight() {
+        return height;
+    }
 }
